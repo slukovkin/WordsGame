000c44c431c7612435b247c4438463a5317cff14
Sergey Lukovkin <slukovkin@icloud.com>
2024-04-10 02:23:06 +0300

    Initial Commit

---
+++ WordsGame/WordsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsGameApp.swift
+//  WordsGame
+//
+//  Created by Sergey Lukovkin on 10.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
